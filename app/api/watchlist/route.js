1d98664b061cee5dc8f687e66219a2b941a6ce00
mrbubbles-src <manuel_fahrenholz@web.de>
2025-04-09 01:29:42 +0200

    feat(api): implement watchlist management endpoints for GET, PUT, and DELETE operations
    Add functionality to retrieve, update, and delete items from the user's watchlist.
    This includes handling authorization tokens, validating user access, and providing
    appropriate success and error messages for each operation.

---
+++ app/api/watchlist/route.js
@@ -0,0 +1,108 @@
+import { getDb } from '@/lib/lowdb';
+import { NextResponse } from 'next/server';
+
+export async function GET(req) {
+  const token = req.headers.get('authorization');
+  const db = await getDb();
+  const { users } = db.data;
+
+  const user = users.find((user) => user.token === token);
+
+  if (!user) {
+    return NextResponse.json(
+      { msg: 'Could not retrieve watchlist, please try again.' },
+      { status: 400 },
+    );
+  }
+
+  const { watchlist } = user;
+
+  return NextResponse.json({ watchlist }, { status: 200 });
+}
+export async function PUT(req) {
+  const body = await req.json();
+  const token = req.headers.get('authorization');
+  const { watchlistItem } = body;
+  const db = await getDb();
+  const { users } = db.data;
+
+  const user = users.find((user) => user.token === token);
+
+  if (!user) {
+    return NextResponse.json(
+      { msg: 'Could not update watchlist, please try again.' },
+      { status: 400 },
+    );
+  }
+
+  const { watchlist } = user;
+  const isAlreadyOnWatchlist = watchlist.find((item) => item === watchlistItem);
+
+  if (isAlreadyOnWatchlist) {
+    return NextResponse.json(
+      { msg: 'Item is already on your Watchlist.' },
+      { status: 400 },
+    );
+  }
+
+  try {
+    watchlist.push(watchlistItem);
+    await db.write();
+    return NextResponse.json(
+      {
+        msg: `${watchlistItem.title} successfully added to Watchlist.`,
+        watchlist,
+      },
+      { status: 201 },
+    );
+  } catch (error) {
+    return NextResponse.json(
+      {
+        msg: `Something went wrong. Please try adding ${watchlistItem.title} again.`,
+        error,
+      },
+      { status: 500 },
+    );
+  }
+}
+export async function DELETE(req) {
+  const body = await req.json();
+  const token = req.headers.get('authorization');
+  const { watchlistItemId } = body;
+  const db = await getDb();
+  const { users } = db.data;
+
+  const user = users.find((user) => user.token === token);
+
+  if (!user) {
+    return NextResponse.json(
+      {
+        msg: 'Could not remove requested item, please try again.',
+      },
+      { status: 400 },
+    );
+  }
+
+  try {
+    const { watchlist } = user;
+    user.watchlist = user.watchlist.filter(
+      (item) => item.id !== watchlistItemId,
+    );
+    await db.write();
+    return NextResponse.json(
+      {
+        msg: 'Successfully deleted item from Watchlist.',
+        watchlist,
+      },
+      { status: 201 },
+    );
+  } catch (error) {
+    return NextResponse.json(
+      {
+        msg: `Something went wrong. Please try removing it again.`,
+        error,
+      },
+      { status: 500 },
+    );
+  }
+}

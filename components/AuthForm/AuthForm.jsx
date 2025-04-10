18179b35433d3a8f6450fdac5fa7156b7ad907ef
mrbubbles-src <manuel_fahrenholz@web.de>
2025-04-10 22:57:16 +0200

    feat(auth): implement authentication form for login and registration
    - Add AuthForm component to handle user authentication.
    - Include fields for username, password, and email (for registration).
    - Implement API calls for login and registration, managing success and error states.
    - Store user data in local storage upon successful authentication.

---
+++ components/AuthForm/AuthForm.jsx
@@ -0,0 +1,113 @@
+'use client';
+
+import { useRouter } from 'next/navigation';
+import { useState } from 'react';
+
+const AuthForm = ({ type }) => {
+  const [error, setError] = useState(false);
+  const router = useRouter();
+  const handleAuthRequest = async (bodyData) => {
+    setError(false);
+    try {
+      const response = await fetch(`/api/${type}`, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(bodyData),
+      });
+      const data = await response.json();
+      return data;
+    } catch (err) {
+      setError(true);
+      return err;
+    }
+  };
+
+  const onAuthSuccess = (resData) => {
+    const user = {
+      username: resData.username,
+      token: resData.token,
+      watchlist: resData.watchlist,
+    };
+
+    localStorage.setItem('user', JSON.stringify(user));
+
+    return router.push('/');
+  };
+
+  const onSubmitHandler = async (e) => {
+    e.preventDefault();
+    const username = e.target.username.value;
+    const password = e.target.password.value;
+    switch (type) {
+      case 'login':
+        const loginCredentials = { username, password };
+        const loginResponse = await handleAuthRequest(loginCredentials);
+        if (loginResponse.token) {
+          onAuthSuccess(loginResponse);
+        } else {
+          setError(true);
+        }
+        break;
+      case 'register':
+        const email = e.target.email.value;
+        const registerCredentials = { email, username, password };
+        const registerResponse = await handleAuthRequest(registerCredentials);
+        if (registerResponse.token) {
+          onAuthSuccess(registerResponse);
+        } else {
+          setError(true);
+        }
+        break;
+
+      default:
+        setError(true);
+        console.warn('Unsupported auth type');
+        break;
+    }
+  };
+  return (
+    <section>
+      <h1>{type && type.at(0).toUpperCase() + type.slice(1)}</h1>
+      <form onSubmit={onSubmitHandler}>
+        {type === 'register' && (
+          <div>
+            <label htmlFor="email">Email</label>
+            <input
+              type="email"
+              name="email"
+              id="email"
+              placeholder="Please enter a E-mail."
+              required
+            />
+          </div>
+        )}
+        <div>
+          <label htmlFor="username">Username</label>
+          <input
+            type="text"
+            name="username"
+            id="username"
+            placeholder="Please enter a Username"
+            minLength={3}
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            name="password"
+            id="password"
+            placeholder="Please enter a Password"
+            minLength={8}
+            required
+          />
+        </div>
+        <button type="submit">{type === 'login' ? 'Login' : 'Register'}</button>
+      </form>
+      {error && <h2>{`Unable to ${type}. Please check your credentials.`}</h2>}
+    </section>
+  );
+};
+
+export default AuthForm;

33b905f1a840e6be1b1fa0c70095f33fe30d244c
mrbubbles-src <manuel_fahrenholz@web.de>
2025-04-10 22:57:21 +0200

    feat(login): create login page with authentication form

---
+++ app/login/page.jsx
@@ -0,0 +1,11 @@
+import AuthForm from '@/components/AuthForm/AuthForm';
+
+const LoginPage = () => {
+  return (
+    <>
+      <AuthForm type={'login'} />
+    </>
+  );
+};
+
+export default LoginPage;

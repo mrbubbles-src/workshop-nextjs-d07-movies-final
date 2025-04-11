36e24d97c40ff3ce0745b09840fb1377465edd1a
mrbubbles-src <manuel_fahrenholz@web.de>
2025-04-12 01:44:38 +0200

    feat(Carousel): add new Carousel component with FontAwesome icons for navigation

---
+++ components/layout/Carousel.jsx
@@ -0,0 +1,9 @@
+import {
+  faCircleChevronRight,
+  faCircleChevronLeft,
+} from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+const Carousel = ({ children }) => {};
+
+export default Carousel;

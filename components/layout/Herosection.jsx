61f66ba570357b1ba55f60ed9e721a0da78a8b96
mrbubbles-src <manuel_fahrenholz@web.de>
2025-04-11 23:33:43 +0200

    feat(Herosection): add Herosection component with header and introductory text

---
+++ components/layout/Herosection.jsx
@@ -0,0 +1,16 @@
+const Herosection = () => {
+  return (
+    <header>
+      <div>
+        <h1>Die D07 Movie & TV DB – Einfach suchen, merken, anschauen.</h1>
+        <p>
+          Durchsuche eine riesige Auswahl an Filmen & Serien mit nur einem Klick
+          und füge deine Favoriten zu deiner Watchlist hinzu.
+        </p>
+        <button>Jetzt entdecken</button>
+      </div>
+    </header>
+  );
+};
+
+export default Herosection;

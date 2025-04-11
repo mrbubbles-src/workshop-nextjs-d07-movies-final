0d30a456880449116cbe642a160031db092e01ec
mrbubbles-src <manuel_fahrenholz@web.de>
2025-04-11 23:50:11 +0200

    feat(LoginPage, RegisterPage): add navigation links for account registration and login

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -1,9 +1,13 @@
 import AuthForm from '@/components/auth/AuthForm';
+import Link from 'next/link';
 
 const LoginPage = () => {
   return (
     <>
       <AuthForm type={'login'} />
+      <Link href={'/register'}>
+        Noch keinen Account? Hier geht es zur Registrierung!
+      </Link>
     </>
   );
 };
--- app/register/page.jsx
+++ app/register/page.jsx
@@ -1,9 +1,13 @@
 import AuthForm from '@/components/auth/AuthForm';
+import Link from 'next/link';
 
 const RegisterPage = () => {
   return (
     <>
       <AuthForm type={'register'} />
+      <Link href={'/login'}>
+        Du hast schon einen Account? Hier geht es zum Login!
+      </Link>
     </>
   );
 };

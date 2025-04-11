6762b08107c123cf5f27b0d5d2711b42bd3138bf
mrbubbles-src <manuel_fahrenholz@web.de>
2025-04-12 01:44:56 +0200

    refactor(Carousel): remove unused FontAwesome icon imports

--- components/layout/Carousel.jsx
+++ components/layout/Carousel.jsx
@@ -1,9 +1,3 @@
-import {
-  faCircleChevronRight,
-  faCircleChevronLeft,
-} from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-
 const Carousel = ({ children }) => {};
 
 export default Carousel;

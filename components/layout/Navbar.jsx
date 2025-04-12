ef6b5b9ab6437bddeb1384d86565a2914df87595
mrbubbles-src <manuel_fahrenholz@web.de>
2025-04-12 22:53:49 +0200

    refactor(Navbar): restructure logo link for improved accessibility and layout

--- components/layout/Navbar.jsx
+++ components/layout/Navbar.jsx
@@ -1,16 +1,20 @@
 import Image from 'next/image';
 import NavLink from '../ui/NavLink/NavLink';
+import Link from 'next/link';
 
 const Navbar = () => {
   return (
     <nav className="flex flex-wrap place-items-center gap-5 bg-amber-600 p-5">
-      <aside className="relative h-20 w-40">
-        <Image
-          src={'/images/logo.svg'}
-          alt='Brand Logo "D07 Movie & TV DB"'
-          fill
-          sizes="100%"
-        />
+      <aside className="h-20 w-40">
+        <Link href="/" className="relative block h-full w-full">
+          <Image
+            src="/images/logo.svg"
+            alt='Brand Logo "D07 Movie & TV DB"'
+            fill
+            priority
+            sizes="100%"
+          />
+        </Link>
       </aside>
       <NavLink href={'/'}>Home</NavLink>
       <NavLink href={'/search'}>Search</NavLink>

78fa61aacb2b2c00a61aabd7df8855b63eea7b88
mrbubbles-src <manuel_fahrenholz@web.de>
2025-04-12 18:45:57 +0200

    feat: add ContentPage, ContentDetailPage, and SearchPage components
    - Introduced new components for content detail, content listing, and search functionality.
    - Each component renders a simple heading within a div, serving as a foundation for further development.

---
+++ app/content/[slug]/page.jsx
@@ -0,0 +1,9 @@
+const ContentDetailPage = () => {
+  return (
+    <div>
+      <h1>ContentDetailPage</h1>
+    </div>
+  );
+};
+
+export default ContentDetailPage;
---
+++ app/content/page.jsx
@@ -0,0 +1,9 @@
+const ContentPage = () => {
+  return (
+    <div>
+      <h1>ContentPage</h1>
+    </div>
+  );
+};
+
+export default ContentPage;

99df43d64e64b0a00b3719c4bc8591e032e8bdad
mrbubbles-src <manuel_fahrenholz@web.de>
2025-04-12 19:53:03 +0200

    refactor(Herosection): enhance layout and styling for improved presentation

--- components/layout/Herosection.jsx
+++ components/layout/Herosection.jsx
@@ -2,15 +2,28 @@ import Link from 'next/link';
 
 const Herosection = () => {
   return (
-    <header>
-      <div>
-        <h1>Die D07 Movie & TV DB – Einfach suchen, merken, anschauen.</h1>
-        <p>
-          Durchsuche eine riesige Auswahl an Filmen & Serien mit nur einem Klick
-          und füge deine Favoriten zu deiner Watchlist hinzu.
-        </p>
-        <Link href={'#search'}>Jetzt entdecken</Link>
-      </div>
+    <header className="flex flex-col place-items-center bg-blue-400 px-[clamp(1rem,5vw,5rem)] py-[clamp(2rem,10vw,10rem)] text-pretty">
+      <article className="flex flex-col gap-5 md:max-w-[75vw] lg:max-w-[55vw]">
+        <section>
+          <h1 className="text-[clamp(2.6rem,6vw,5.2rem)] font-bold">
+            D07 Movie & TV DB
+          </h1>
+          <h2 className="text-[clamp(1.3rem,3vw,2.6rem)] font-bold">
+            Einfach suchen, merken, anschauen.
+          </h2>
+        </section>
+        <section>
+          <p className="text-[clamp(1.1rem,3vw,2rem)]">
+            Durchsuche eine riesige Auswahl an Filmen & Serien mit nur einem
+            Klick und füge deine Favoriten zu deiner Watchlist hinzu.
+          </p>
+        </section>
+        <Link
+          href="#search"
+          className="w-fit rounded-md bg-green-400 px-[clamp(1rem,2.5vw,2rem)] py-[clamp(0.5rem,1.5vw,1rem)] text-[clamp(1rem,2vw,1.5rem)] font-bold text-white transition-colors hover:bg-green-500">
+          Jetzt entdecken
+        </Link>
+      </article>
     </header>
   );
 };

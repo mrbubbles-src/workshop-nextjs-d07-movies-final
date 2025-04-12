2781e2b0cde84de970263efb9897f17cc056ed55
mrbubbles-src <manuel_fahrenholz@web.de>
2025-04-12 20:47:35 +0200

    feat(Search): implement search input and form submission for enhanced user interaction

--- components/content/search/Search.jsx
+++ components/content/search/Search.jsx
@@ -1,5 +1,32 @@
+'use client';
+
+import { useSearch } from '@/context/SearchProvider';
+import { useRouter } from 'next/navigation';
+
 const Search = () => {
-  return <div></div>;
+  const { loading, setLoading, searchValue, setSearchValue } = useSearch();
+  const router = useRouter();
+  const submitHandler = (e) => {
+    e.preventDefault();
+  };
+  return (
+    <section id="search" className="p-3">
+      <form onSubmit={submitHandler} className="flex justify-center border-2">
+        <input
+          type="search"
+          name="search"
+          placeholder="z. B. The Matrix"
+          id="search-input"
+          className="w-[30rem] border-2 p-2"
+          value={searchValue}
+          onChange={(e) => setSearchValue(e.target.value)}
+        />
+        <button type="submit" className="border-2 p-2">
+          🔎
+        </button>
+      </form>
+    </section>
+  );
 };
 
 export default Search;

4623e7154b9359db6c776f412a2976993bebe74b
mrbubbles-src <manuel_fahrenholz@web.de>
2025-04-12 18:46:16 +0200

    refactor(Navbar): restructure layout and improve logo handling in Navbar component

--- components/layout/Navbar.jsx
+++ components/layout/Navbar.jsx
@@ -3,14 +3,19 @@ import NavLink from '../ui/NavLink/NavLink';
 
 const Navbar = () => {
   return (
-    <nav className="bg-amber-600 p-5">
-      <Image
-        src={'images/logo.svg'}
-        alt='Brand Logo "D07 Movie & TV DB"'
-        width={1024}
-        height={1024}
-      />
+    <nav className="flex flex-wrap place-items-center gap-5 bg-amber-600 p-5">
+      <aside className="relative h-20 w-40">
+        <Image
+          src={'/images/logo.svg'}
+          alt='Brand Logo "D07 Movie & TV DB"'
+          fill
+          sizes="100%"
+        />
+      </aside>
       <NavLink href={'/'}>Home</NavLink>
+      <NavLink href={'/search'}>Search</NavLink>
+      <NavLink href={'/content'}>Content</NavLink>
+      <NavLink href={'/content/slug'}>Content Slug</NavLink>
       <NavLink href={'/login'}>Login</NavLink>
       <NavLink href={'/register'}>Register</NavLink>
     </nav>

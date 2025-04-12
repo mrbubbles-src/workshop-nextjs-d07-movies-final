3cd809d3125cf71ebcf9b90ad98af2961d1a0f68
mrbubbles-src <manuel_fahrenholz@web.de>
2025-04-12 20:47:26 +0200

    feat(Home): add Search and SearchPreview components for enhanced search functionality

--- app/page.jsx
+++ app/page.jsx
@@ -1,10 +1,14 @@
 import PopularNow from '@/components/content/PopularNow';
 import Herosection from '@/components/layout/Herosection';
+import Search from '@/components/content/search/Search';
+import SearchPreview from '@/components/content/search/SearchPreview';
 
 export default function Home() {
   return (
     <>
       <Herosection />
+      <Search />
+      <SearchPreview />
       <PopularNow type={'movies'} />
       <PopularNow type={'tvShows'} />
     </>

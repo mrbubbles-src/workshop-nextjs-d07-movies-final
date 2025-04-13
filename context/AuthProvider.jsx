5ced3eb4c316ee3cd11cfc3fcd506c69c52d12e8
mrbubbles-src <manuel_fahrenholz@web.de>
2025-04-13 02:15:48 +0200

    feat(AuthProvider): implement user fetching logic on token presence for authentication context

--- context/AuthProvider.jsx
+++ context/AuthProvider.jsx
@@ -4,7 +4,42 @@ import { createContext, useContext, useEffect, useState } from 'react';
 
 const AuthContext = createContext(undefined);
 export const AuthProvider = ({ children }) => {
-  return <AuthContext.Provider value={{}}>{children}</AuthContext.Provider>;
+  const [user, setUser] = useState(null);
+  useEffect(() => {
+    const token = localStorage.getItem('token');
+
+    if (!token) return;
+
+    const parsedToken = JSON.parse(token);
+
+    const fetchUser = async () => {
+      try {
+        const res = await fetch('/api/watchlist', {
+          headers: {
+            Authorization: parsedToken.token,
+          },
+        });
+        if (!res.ok) throw new Error(res.msg);
+
+        const userData = await res.json();
+
+        console.log(userData);
+
+        setUser({ ...userData, token });
+      } catch (err) {
+        console.error(err);
+        setUser(null);
+      }
+    };
+
+    fetchUser();
+  }, []);
+
+  return (
+    <AuthContext.Provider value={{ user, setUser }}>
+      {children}
+    </AuthContext.Provider>
+  );
 };
 
 export const useAuth = () => {

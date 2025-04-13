26f04e8ce1552d1fb9e8d91fab5250d99588467c
mrbubbles-src <manuel_fahrenholz@web.de>
2025-04-13 02:37:12 +0200

    feat(Navbar): add Navbar component for site navigation with logo and links

--- components/layout/Navbar/Navbar.jsx
+++ components/layout/Navbar/Navbar.jsx
@@ -1,10 +1,11 @@
 import Image from 'next/image';
-import NavLink from '../ui/NavLink/NavLink';
+import NavLink from '../../ui/NavLink/NavLink';
 import Link from 'next/link';
+import Auth from './Auth';
 
 const Navbar = () => {
   return (
-    <nav className="flex flex-wrap place-items-center gap-5 bg-amber-600 p-5">
+    <nav className="flex flex-wrap items-center justify-between p-5">
       <aside className="h-20 w-40">
         <Link href="/" className="relative block h-full w-full">
           <Image
@@ -16,12 +17,11 @@ const Navbar = () => {
           />
         </Link>
       </aside>
-      <NavLink href={'/'}>Home</NavLink>
-      <NavLink href={'/search'}>Search</NavLink>
-      <NavLink href={'/content'}>Content</NavLink>
-      <NavLink href={'/content/slug'}>Content Slug</NavLink>
-      <NavLink href={'/login'}>Login</NavLink>
-      <NavLink href={'/register'}>Register</NavLink>
+      <aside className="flex h-20 w-40 items-center gap-5">
+        <NavLink href={'/dashboard'}>Dashboard</NavLink>
+        <NavLink href={'/search'}>Suche</NavLink>
+      </aside>
+      <Auth />
     </nav>
   );
 };
